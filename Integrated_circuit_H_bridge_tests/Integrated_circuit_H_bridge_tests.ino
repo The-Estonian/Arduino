fbd52058fccfc67218f2c94da5d7cf3e93058d93
The-Estonian <Zaar2213@gmail.com>
2023-03-07 15:39:15 +0200

    finished h bridge project successfully

--- Integrated_circuit_H_bridge_tests/Integrated_circuit_H_bridge_tests.ino
+++ Integrated_circuit_H_bridge_tests/Integrated_circuit_H_bridge_tests.ino
@@ -2,8 +2,13 @@ const int potePin = A0;
 const int direction = 4;
 const int onOff = 5;
 const int enablePin = 9;
-const controlPin1 = 2;
-const controlPin2 = 3;
+const int controlPin1 = 2;
+const int controlPin2 = 3;
+
+int onOffState = 0;
+int previousOnOffState = 0;
+int directionState = 0;
+int previousDirectionState = 0;
 
 int motorEnabled = 0;
 int motorSpeed = 0;
@@ -20,9 +25,42 @@ void setup() {
 }
 
 void loop() {
-  int poteValue = analogRead(potePin);
-  int directionValue = digitalRead(direction);
-  int onOffValue = digitalRead(onOff);
+  int motorSpeed = analogRead(potePin);
+  int directionState = digitalRead(direction);
+  int onOffState = digitalRead(onOff);
   delay(1);
+  if (onOffState != previousOnOffState) {
+    if (onOffState == HIGH) {
+      motorEnabled = !motorEnabled;
+      Serial.println("Motor enabled!");
+    }
+  }
 
+  if (directionState != previousDirectionState) {
+    if (directionState == HIGH) {
+      motorDirection = !motorDirection;
+      Serial.println("Motor direction adjust!");
+    }
+  }
+
+  if (motorDirection == 1) {
+    Serial.println("Motor direction forward!");
+    digitalWrite(controlPin1, LOW);
+    digitalWrite(controlPin2, HIGH);
+  } else {
+    Serial.println("Motor direction backwards!");
+    digitalWrite(controlPin1, HIGH);
+    digitalWrite(controlPin2, LOW);
+  }
+
+  if (motorEnabled == 1) {
+    analogWrite(enablePin, motorSpeed);
+    Serial.println("Motor activated!");
+  } else {
+    Serial.println("Motor disabled!");
+    analogWrite(enablePin, 0);
+  }
+
+  previousOnOffState = onOffState;
+  previousDirectionState = directionState;
 }
